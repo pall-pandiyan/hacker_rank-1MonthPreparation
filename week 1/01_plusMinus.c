38af3eace830321813c5a78db190d7bd6477016e
pall-pandiyan <pallpandiyan@gmail.com>
2022-05-23 17:53:07 +0530

    solved

--- week 1/01_plusMinus.c
+++ week 1/01_plusMinus.c
@@ -1,13 +1,13 @@
-#include assert.h
-#include ctype.h
-#include limits.h
-#include math.h
-#include stdbool.h
-#include stddef.h
-#include stdint.h
-#include stdio.h
-#include stdlib.h
-#include string.h
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char readline();
 char ltrim(char);
