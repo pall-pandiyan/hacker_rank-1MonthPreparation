69544334a8412981cc9f0f6f5bd36e39738355c9
pall-pandiyan <pallpandiyan@gmail.com>
2022-05-27 16:52:43 +0530

    added a sample input and output

--- week 1/01_plusMinus.c
+++ week 1/01_plusMinus.c
@@ -9,41 +9,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-char readline();
-char ltrim(char);
-char rtrim(char);
-char split_string(char);
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
 
-int parse_int(char);
+int parse_int(char*);
 
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
 
-  Complete the 'plusMinus' function below.
- 
-  The function accepts INTEGER_ARRAY arr as parameter.
- 
-
-void plusMinus(int arr_count, int arr) {
+void plusMinus(int arr_count, int* arr) {
     double zeroCount = 0, posCount = 0, negCount = 0;
 
-    for (int i=0; iarr_count; i++) {
+    for (int i=0; i<arr_count; i++) {
         if (arr[i] == 0) {
             zeroCount++;
-        } else if (arr[i]  0) {
+        } else if (arr[i] > 0) {
             posCount++;
-        } else if (arr[i]  0){
+        } else if (arr[i] < 0){
             negCount++;
         }
     }
-    posCount = posCountarr_count;
-    negCount = negCountarr_count;
-    zeroCount = zeroCountarr_count;
+    posCount = posCount/arr_count;
+    negCount = negCount/arr_count;
+    zeroCount = zeroCount/arr_count;
     
     if (zeroCount+posCount+negCount == 1.0) {
-        printf(%lfn, posCount);
-        printf(%lfn, negCount);
-        printf(%lfn, zeroCount);
+        printf("%lf\n", posCount);
+        printf("%lf\n", negCount);
+        printf("%lf\n", zeroCount);
     } else {
-        printf(Error!n);  
+        printf("Error!\n");  
     }
 }
 
@@ -51,14 +51,14 @@ int main()
 {
     int n = parse_int(ltrim(rtrim(readline())));
 
-    char arr_temp = split_string(rtrim(readline()));
+    char** arr_temp = split_string(rtrim(readline()));
 
-    int arr = malloc(n  sizeof(int));
+    int* arr = malloc(n * sizeof(int));
 
-    for (int i = 0; i  n; i++) {
-        int arr_item = parse_int((arr_temp + i));
+    for (int i = 0; i < n; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
 
-        (arr + i) = arr_item;
+        *(arr + i) = arr_item;
     }
 
     plusMinus(n, arr);
@@ -66,15 +66,15 @@ int main()
     return 0;
 }
 
-char readline() {
+char* readline() {
     size_t alloc_length = 1024;
     size_t data_length = 0;
 
-    char data = malloc(alloc_length);
+    char* data = malloc(alloc_length);
 
     while (true) {
-        char cursor = data + data_length;
-        char line = fgets(cursor, alloc_length - data_length, stdin);
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
 
         if (!line) {
             break;
@@ -82,86 +82,86 @@ char readline() {
 
         data_length += strlen(cursor);
 
-        if (data_length  alloc_length - 1  data[data_length - 1] == 'n') {
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
             break;
         }
 
-        alloc_length = 1;
+        alloc_length <<= 1;
 
         data = realloc(data, alloc_length);
 
         if (!data) {
-            data = '0';
+            data = '\0';
 
             break;
         }
     }
 
-    if (data[data_length - 1] == 'n') {
-        data[data_length - 1] = '0';
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
 
         data = realloc(data, data_length);
 
         if (!data) {
-            data = '0';
+            data = '\0';
         }
     } else {
         data = realloc(data, data_length + 1);
 
         if (!data) {
-            data = '0';
+            data = '\0';
         } else {
-            data[data_length] = '0';
+            data[data_length] = '\0';
         }
     }
 
     return data;
 }
 
-char ltrim(char str) {
+char* ltrim(char* str) {
     if (!str) {
-        return '0';
+        return '\0';
     }
 
-    if (!str) {
+    if (!*str) {
         return str;
     }
 
-    while (str != '0' && isspace(str)) {
+    while (*str != '\0' && isspace(*str)) {
         str++;
     }
 
     return str;
 }
 
-char rtrim(char str) {
+char* rtrim(char* str) {
     if (!str) {
-        return '0';
+        return '\0';
     }
 
-    if (!str) {
+    if (!*str) {
         return str;
     }
 
-    char end = str + strlen(str) - 1;
+    char* end = str + strlen(str) - 1;
 
-    while (end = str && isspace(end)) {
+    while (end >= str && isspace(*end)) {
         end--;
     }
 
-    (end + 1) = '0';
+    *(end + 1) = '\0';
 
     return str;
 }
 
-char split_string(char str) {
-    char splits = NULL;
-    char token = strtok(str,  );
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
 
     int spaces = 0;
 
     while (token) {
-        splits = realloc(splits, sizeof(char)  ++spaces);
+        splits = realloc(splits, sizeof(char*) * ++spaces);
 
         if (!splits) {
             return splits;
@@ -169,19 +169,29 @@ char split_string(char str) {
 
         splits[spaces - 1] = token;
 
-        token = strtok(NULL,  );
+        token = strtok(NULL, " ");
     }
 
     return splits;
 }
 
-int parse_int(char str) {
-    char endptr;
+int parse_int(char* str) {
+    char* endptr;
     int value = strtol(str, &endptr, 10);
 
-    if (endptr == str  endptr != '0') {
+    if (endptr == str || *endptr != '\0') {
         exit(EXIT_FAILURE);
     }
 
     return value;
 }
+
+
+// the output will be...
+
+// 6
+// -4 3 -9 0 4 1
+
+// 0.500000
+// 0.333333
+// 0.166667
